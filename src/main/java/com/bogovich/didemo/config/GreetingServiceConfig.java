c983e6dc9824e40e50af084ba95e5bf37df60691
chillten <bogovich.alexander@gmail.com>
2017-08-05 16:31:48 +0700

    Implement ServiceFactory and map bean in java config

---
+++ src/main/java/com/bogovich/didemo/config/GreetingServiceConfig.java
@@ -0,0 +1,40 @@
+package com.bogovich.didemo.config;
+
+import com.bogovich.didemo.services.GreetingRepository;
+import com.bogovich.didemo.services.GreetingService;
+import com.bogovich.didemo.services.GreetingServiceFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+public class GreetingServiceConfig {
+
+    @Bean
+    GreetingServiceFactory greetingServiceFactory(GreetingRepository greetingRepository){
+        return new GreetingServiceFactory(greetingRepository);
+    }
+
+    @Bean
+    @Primary
+    @Profile({"en", "default"})
+    GreetingService primaryGreetingService(GreetingServiceFactory greetingServiceFactory){
+        return greetingServiceFactory.createGreetingService("en");
+    }
+
+    @Bean
+    @Primary
+    @Profile({"es"})
+    GreetingService primarySpanishGreetingService(GreetingServiceFactory greetingServiceFactory){
+        return greetingServiceFactory.createGreetingService("es");
+    }
+
+    @Bean
+    @Primary
+    @Profile({"de"})
+    GreetingService primaryGermanGreetingService(GreetingServiceFactory greetingServiceFactory){
+        return greetingServiceFactory.createGreetingService("de");
+    }
+
+}
